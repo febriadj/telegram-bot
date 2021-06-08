b7f54dd6c41147532f085a4180765f7651993bbc
febriadj <iamfebriadji@gmail.com>
2021-06-08 23:04:52 +0700

    add bot.ts

---
+++ build/bot.js
@@ -0,0 +1,51 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const telegraf_1 = require("telegraf");
+const speed = [
+    1500, 2000, 2500, 3000, 3500,
+    4000, 4500, 5000, 5500, 6000,
+    6500, 7000, 7500, 8000, 8500
+];
+const inSpeed = Math.floor(Math.random() * speed.length);
+const bot = new telegraf_1.Telegraf(process.env.BOT_TOKEN);
+bot.start(ctx => ctx.reply("Hi " + ctx.from.first_name + ", selamat datang"));
+(function () {
+    const forbWord = process.env.FORBIDDEN_WORD.split(", "), strForbWord = forbWord.join("|");
+    bot.hears(new RegExp(strForbWord, "gi"), (ctx) => __awaiter(this, void 0, void 0, function* () {
+        yield ctx.deleteMessage();
+        ctx.reply("@" + ctx.from.username + " terdeteksi menggunakan kata-kata terlarang");
+        const res = ["Hindari penggunaan kata-kata kasar", "Kasar amat lu", "Dilarang berkata kasar"];
+        const index = yield Math.floor(Math.random() * res.length);
+        setTimeout(() => ctx.reply(res[index]), speed[inSpeed]);
+    }));
+})();
+(function () {
+    const reg = /(?=.*hello|hei|hai|hi|halo)(?=.*ninja).*/im;
+    bot.hears(new RegExp(reg), (ctx) => __awaiter(this, void 0, void 0, function* () {
+        const res = ["Hi " + ctx.from.first_name, "Hello", "Hei", "Hai", "Hi", "Halo"], index = yield Math.floor(Math.random() * res.length);
+        setTimeout(() => ctx.reply(res[index]), speed[inSpeed]);
+    }));
+})();
+(function () {
+    const reg = /(?=.*?)(?=.*ninja)(?=.*sedang apa|.*lagi [apa|ngapain]).*/im;
+    bot.hears(new RegExp(reg), (ctx) => __awaiter(this, void 0, void 0, function* () {
+        const res = ["Lagi rebahan aja nih dikamar", "Lagi godain cewek", "Lagi nugas nih, puyeng gua gk kelar-kelar"], index = yield Math.floor(Math.random() * res.length);
+        setTimeout(() => ctx.reply(res[index]), speed[inSpeed]);
+        setTimeout(() => {
+            index === 1 ? ctx.reply("Mau cewek gk lu? @" + ctx.from.username) : null;
+        }, speed[inSpeed] + speed[inSpeed]);
+    }));
+})();
+bot.on("sticker", ctx => {
+    setTimeout(() => ctx.reply("👍"), speed[inSpeed]);
+});
+exports.default = bot;
---
+++ src/bot.ts
@@ -0,0 +1,62 @@
+import { Telegraf } from "telegraf";
+
+const speed = [
+  1500, 2000, 2500, 3000, 3500, 
+  4000, 4500, 5000, 5500, 6000, 
+  6500, 7000, 7500, 8000, 8500
+];
+
+const inSpeed = Math.floor(Math.random() * speed.length);
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start(ctx => ctx.reply("Hi " + ctx.from.first_name + ", selamat datang"));
+
+(function(): void {
+  const 
+    forbWord: Array<string> = process.env.FORBIDDEN_WORD.split(", "),
+    strForbWord: string = forbWord.join("|");
+
+  bot.hears(new RegExp(strForbWord, "gi"), async (ctx): Promise<void> => {
+    await ctx.deleteMessage();
+  
+    ctx.reply("@" + ctx.from.username + " terdeteksi menggunakan kata-kata terlarang");
+  
+    const res: Array<string> = ["Hindari penggunaan kata-kata kasar", "Kasar amat lu", "Dilarang berkata kasar"];
+    const index: number = await Math.floor(Math.random() * res.length);
+
+    setTimeout(() => ctx.reply(res[index]), speed[inSpeed]);
+  })
+})();
+
+(function(): void {
+  const reg: RegExp = /(?=.*hello|hei|hai|hi|halo)(?=.*ninja).*/im;
+
+  bot.hears(new RegExp(reg), async (ctx): Promise<void> => {
+    const 
+      res: Array<string> = ["Hi " + ctx.from.first_name, "Hello", "Hei", "Hai", "Hi", "Halo"]
+    , index: number = await Math.floor(Math.random() * res.length);
+  
+    setTimeout(() => ctx.reply(res[index]), speed[inSpeed]);
+  });
+})();
+
+(function(): void {
+  const reg: RegExp = /(?=.*?)(?=.*ninja)(?=.*sedang apa|.*lagi [apa|ngapain]).*/im;
+  
+  bot.hears(new RegExp(reg), async (ctx): Promise<void> => {
+    const
+      res: Array<string> = ["Lagi rebahan aja nih dikamar", "Lagi godain cewek", "Lagi nugas nih, puyeng gua gk kelar-kelar"]
+    , index: number = await Math.floor(Math.random() * res.length);
+
+    setTimeout(() => ctx.reply(res[index]), speed[inSpeed]);
+    setTimeout(() => {
+      index === 1 ? ctx.reply("Mau cewek gk lu? @" + ctx.from.username) : null;
+    }, speed[inSpeed] + speed[inSpeed]);
+  })
+})();
+
+bot.on("sticker", ctx => {
+  setTimeout(() => ctx.reply("👍"), speed[inSpeed]);
+});
+
+export default bot;
