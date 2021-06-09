994324438f742e50e9fa3173b7dce00136d7d69a
febriadj <iamfebriadji@gmail.com>
2021-06-09 15:24:38 +0700

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,15 @@ dotenv.config({ path: "./.env" });
 import * as express from "express";
 import bot from "./bot";
 
-bot.telegram.setWebhook(`https://ninjadigitalbot.herokuapp.com/bot${process.env.BOT_TOKEN}`);
-
 const app = express();
 const port = process.env.PORT || 3000;
 
+bot.telegram.setWebhook(`https://ninjadigitalbot.herokuapp.com/bot${process.env.BOT_TOKEN}`);
+
 app.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
+app.get('/', (req, res) => res.send('hello world'));
 
 bot.launch(); 
-console.log("bot telah aktif");
 
 app.listen(3000);
 console.log(`[${port}] server listening`);
