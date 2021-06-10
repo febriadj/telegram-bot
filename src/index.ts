57900ec6214a9d45631d0700fe66f1f4666c8c00
febriadj <iamfebriadji@gmail.com>
2021-06-11 02:33:35 +0700

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,5 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 import bot from "./bot";
-import express, { Request, Response } from "express";
-
-bot.telegram.setWebhook(`https://ninjadigitalbot.herokuapp.com/secret`);
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => res.send('hello world'));
-app.use(bot.webhookCallback(`/secret`));
-
-app.listen(port);
-console.log("server listening on port " + port);
 
 bot.launch();
