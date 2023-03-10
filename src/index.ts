4e54d57ca2254952e40145dd7ac920d9112b116b
febriadj <iamfebriadji@gmail.com>
2023-03-10 19:50:16 +0700

    feat(router): add root route

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import webhook from './webhook';
 import bot from './bot';
 import config from './config';
@@ -11,6 +11,13 @@ const port: number = Number(process.env.PORT) || config.port;
 const app: Application = express();
 
 webhook(app, bot);
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'express router works',
+  });
+});
 
 app.listen(port, () => {
   console.log(`[${port}] server running`);
